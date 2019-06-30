cf76784f633917fb7e13026f5ea364d13fcf90f1
Lech <lech.surowka@gmail.com>
2019-06-30 20:32:51 +0200

    ok

---
+++ src/main/java/com/surowka/AirQualityChecker/AirQualityCheckerApplication.java
@@ -0,0 +1,15 @@
+package com.surowka.AirQualityChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityCheckerApplication {
+
+	// git test
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirQualityCheckerApplication.class, args);
+	}
+
+}
